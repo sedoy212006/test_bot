44075b729269f7cafee1bcb148fd6770d27a38ef
sedoy212006 <101042461+sedoy212006@users.noreply.github.com>
2022-10-02 18:14:47 +0300

    Создал файл add.py и добавил структуру
    Создал тестовый  шаблон  ответа бота на любое сообщение ему

---
+++ add.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+token = ""
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def get_message(message: types.Message):
+    chat_id = message.chat.id
+
+    text = "Привет)))"
+
+    sent_message = await bot.send_message(chat_id, text=text)
+
+    sent_message = await bot.send_photo(chat_id,
+                                        photo="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png")
+
+
+executor.start_polling(dp)
+
+# bot.get_updates()
